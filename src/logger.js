5e1eb105c47de59a3e7f39dc89055afa9fa2e923
wemakro <eli@wemakro.com>
2026-06-05 17:52:51 +0300

    Update logger.js

--- src/logger.js
+++ src/logger.js
@@ -1,1 +1,10 @@
+function ts() { return new Date().toISOString(); }
 
+const logger = {
+  info:  (...a) => console.log(`[${ts()}] INFO `, ...a),
+  warn:  (...a) => console.warn(`[${ts()}] WARN `, ...a),
+  error: (...a) => console.error(`[${ts()}] ERROR`, ...a),
+  debug: (...a) => console.log(`[${ts()}] DEBUG`, ...a),
+};
+
+module.exports = logger;
