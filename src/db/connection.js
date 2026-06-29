a638237c2ccae377e5e308fd9590201118033500
wemakro <eli@wemakro.com>
2026-06-29 13:19:13 +0300

    Update connection.js

--- src/db/connection.js
+++ src/db/connection.js
@@ -7,7 +7,7 @@ if (!process.env.DATABASE_URL) {
 }
 
 const pool = new Pool({
-  connectionString: process.env.DATABASE_URL,
+  connectionString: process.env.DATABASE_URL || process.env.POSTGRES_URL,
   ssl: process.env.DATABASE_URL && process.env.DATABASE_URL.includes("render.com")
     ? { rejectUnauthorized: false }
     : false,
